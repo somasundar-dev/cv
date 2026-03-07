16b11cab4180f44a006d2f22feb44bb391b62e61
Somasundar S <somasundar.dev@gmail.com>
2026-03-07 22:53:55 +0530

    Refactor resume data and update UI components for improved presentation and functionality

--- src/src/app/Pages/summary/summary.component.ts
+++ src/src/app/Pages/summary/summary.component.ts
@@ -5,13 +5,13 @@ import { Component, input, Input } from '@angular/core';
   selector: 'app-summary',
   imports: [CommonModule],
   templateUrl: './summary.component.html',
-  styleUrl: './summary.component.css'
+  styleUrl: './summary.component.css',
 })
 export class SummaryComponent {
   @Input() name: string = '';
   @Input() initials: string = '';
   @Input() location: string = '';
-  @Input() locationLink: string = '';
+  @Input() locationLink: string = '';  
   @Input() summary: string = '';
   @Input() about: string = '';
   @Input() avatarUrl: string = '';
@@ -20,4 +20,27 @@ export class SummaryComponent {
   @Input() tel: string = '';
   @Input() githubUrl: string = '';
   @Input() linkedinUrl: string = '';
+  @Input() startDate: string = '';
+
+  get yearsOfExperience(): string {
+    const start = new Date(this.startDate);
+    const now = new Date();
+    let years = now.getFullYear() - start.getFullYear();
+    let months = now.getMonth() - start.getMonth();
+    // Adjust months and years if the end month is before the start month
+    if (months < 0) {
+      years--;
+      months += 12;
+    }
+    let yoe_string =
+      months < 6 ? `${years}+ years` : `around ${years + 1} years`;
+    return yoe_string;
+  }
+
+  get summaryWithExperience(): string {
+    return this.about.replace(
+      '{yearsOfExperience}',
+      this.yearsOfExperience.toString(),
+    );
+  }
 }
--- src/src/app/app.component.ts
+++ src/src/app/app.component.ts
@@ -3,8 +3,7 @@ import { SummaryComponent } from './Pages/summary/summary.component';
 import { EducationComponent } from './Pages/education/education.component';
 import { WorkComponent } from './Pages/work/work.component';
 import { ProjectsComponent } from './Pages/projects/projects.component';
-import { SkillsComponent } from './Pages/skills/skills.component';
-import { CertificatesComponent } from "./Pages/certificates/certificates.component";
+import { CertificatesComponent } from './Pages/certificates/certificates.component';
 import { RESUME_DATA } from './Config/resume-data';
 import { CommonModule } from '@angular/common';
 
@@ -15,14 +14,13 @@ import { CommonModule } from '@angular/common';
     EducationComponent,
     WorkComponent,
     ProjectsComponent,
-    SkillsComponent,
     CertificatesComponent,
-    CommonModule
-],
+    CommonModule,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'cv';
-  Info = RESUME_DATA
+  Info = RESUME_DATA;
 }
