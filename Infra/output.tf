e458c78bbfbcd7bf30bbec4b3e684220ea658178
Somasundar S <somasundar.dev@gmail.com>
2026-03-15 20:43:14 +0530

    Remove obsolete Terraform configuration files for CloudFront and S3 setup

--- Infra/output.tf
+++
@@ -1,3 +0,0 @@
-output "aws_cloudfront_distribution_domain_name" {
-  value = aws_cloudfront_distribution.cloudfront_distribution.domain_name
-}
--- Infra/variables.tf
+++
@@ -1,6 +0,0 @@
-variable "region" {}
-variable "environment" {}
-variable "bucket_name" {}
-variable "app_name" {}
-variable "current_version" {}
-variable "domain_name" {}
